560d23776ceacb4c31de0dc572c1a41bd3404268
vavan19911987 <vavan7520@gmail.com>
2022-12-10 14:31:37 +0300

    довнрстал модалку

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function () {
     "user strict";
-    const swiper = new Swiper('.swiper', {
+    const swiper = new Swiper ('.swiper', {
         loop: true,
         // spaceBetween: 130,
         autoplay: {
@@ -70,17 +70,105 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
 
-    // font-size: 18px;
-    // color: #444444;
-    //
 
-    // let list = document.querySelector('.content__leftBar_list');
-    // let q = document.querySelector('.list');
-    // list.addEventListener('click', function () {
-    //     // q.classList.add('active2');
-    //     console.log(q);
-    //     q.setAttribute("style", "opacity: 1;");
-    // });
+
+    let form = document.querySelector('.feed-form');
+    let formInput = document.querySelectorAll('.input');
+    let inputPhone = document.querySelectorAll('.inputPhone');
+    let inputEmail = document.querySelectorAll('.inputEmail');
+
+
+
+    function validateEmail(email) {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+
+    function validateCountry(country) {
+        let re = new RegExp('.co$');
+        return re.test(String(country).toLowerCase());
+    }
+
+    function validatePhone(phone) {
+        let re = /^[0-9\s]*$/;
+        return re.test(String(phone));
+    }
+
+    form.onsubmit = function () {
+        let emailVal = inputEmail.value,
+            phoneVal = inputPhone.value,
+            emptyInputs = Array.from(formInput).filter(input => input.value === '');
+
+        formInput.forEach(function (input) {
+            if (input.value === '') {
+                input.classList.add('error');
+
+                // document.querySelector('.input').placeholder = 'Нужный текст';
+                let  o =document.querySelectorAll('.input');
+                for (let i = 0; i < o.length; i++) {
+                    o[i].placeholder = 'Поле обязательно для заполнения';
+
+                }
+
+            } else {
+                input.classList.remove('error');
+            }
+        });
+
+        if (emptyInputs.length !== 0) {
+            console.log('inputs not filled');
+            return false;
+        }
+
+        if(!validateEmail(emailVal)) {
+            console.log('email not valid');
+            inputEmail.classList.add('error');
+            return false;
+        } else {
+            inputEmail.classList.remove('error');
+        }
+
+        if (validateCountry(emailVal)) {
+            console.log('email from Columbia');
+            inputEmail.classList.add('error');
+
+            return false;
+        } else {
+            inputEmail.classList.remove('error');
+        }
+
+        if (inputPhone.value === validatePhone(phoneVal)) {
+            console.log('phone not valid');
+            inputPhone.classList.add('error');
+            return false;
+        } else {
+            inputPhone.classList.remove('error');
+        }
+
+
+
+
+    };
+
+
+    let close = document.querySelector('.close');
+    let over = document.querySelector('.overlay');
+    let connection = document.querySelector('.connection');
+
+
+    close.addEventListener('click', function () {
+
+        over.style.display = 'none';
+        console.log('ok');
+    });
+    connection.addEventListener('click', function () {
+        over.style.display = 'block';
+    });
+
+
+
+
 
 
 
